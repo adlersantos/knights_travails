b85125c41d0f2066ff8b5dba2a782ed900686ee8
Adler Santos <adlersantos712@gmail.com>
2014-01-31 14:00:40 +0800

    isolate KnightTravails::Move and added more specs

---
+++ spec/move_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe KnightsTravails::Move do
+  before :each do
+    @move = KnightsTravails::Move.new([0, 0])
+    @move.generate_new_moves
+    @children_values = @move.children.map { |child| child.value }
+  end
+
+  describe "#generate_new_moves" do
+    it "should set only the valid moves to the children's values" do
+      expect(@children_values).to include([1, 2], [2, 1])
+      @children_values.should_not include([-1, -2], [0, 0])
+    end
+
+    it "should set a move's children to be Move objects" do
+      @move.children.each do |child|
+        expect(child.class).to eq(KnightsTravails::Move)
+      end
+    end
+  end
+end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,37 +1,26 @@
 require 'spec_helper'
 
 describe Node do
+  before :each do
+    @a = Node.new(1)
+    @b = Node.new(2, parent: @a)
+    @c = Node.new(3, parent: @a)
+    @d = Node.new(3, parent: @b)
+    @e = Node.new(4, parent: @b)
+    @f = Node.new(5, parent: @c)
+
+    @a.add_child(@b)
+    @a.add_child(@c)
+    @b.add_child(@d)
+    @b.add_child(@e)
+    @c.add_child(@f)
+  end
+
   it "should do depth-first-search correctly" do
-    a = Node.new(1)
-    b = Node.new(2, parent: a)
-    c = Node.new(3, parent: a)
-    d = Node.new(3, parent: b)
-    e = Node.new(4, parent: b)
-    f = Node.new(5, parent: c)
-
-    a.add_child(b)
-    a.add_child(c)
-    b.add_child(d)
-    b.add_child(e)
-    c.add_child(f)
-
-    a.dfs(3).should == d
+    @a.dfs(3).should == @d
   end
 
   it "should do breadth-first-search correctly" do
-    a = Node.new(1)
-    b = Node.new(2, parent: a)
-    c = Node.new(3, parent: a)
-    d = Node.new(3, parent: b)
-    e = Node.new(4, parent: b)
-    f = Node.new(5, parent: c)
-
-    a.add_child(b)
-    a.add_child(c)
-    b.add_child(d)
-    b.add_child(e)
-    c.add_child(f)
-
-    a.bfs(3).should == c
+    @a.bfs(3).should == @c
   end
 end
