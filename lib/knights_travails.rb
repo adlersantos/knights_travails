c2464e00e5b9896f0b95f16446583b75108decc6
Adler Santos <adlersantos712@gmail.com>
2014-01-31 13:37:57 +0800

    added KnightMove class

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -2,5 +2,28 @@ require "knights_travails/node"
 require "knights_travails/version"
 
 module KnightsTravails
-  # Your code goes here...
+  class KnightMove < Node
+    MOVES = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [-2, -1]]
+
+    def x_value
+      @value[0]
+    end
+
+    def y_value
+      @value[1]
+    end
+
+    def generate_new_moves
+      moves = MOVES.collect { |move| [x_value + move[0], y_value + move[1]] }
+      valid_moves = moves.select { |move| validate_position(move) }
+      @children = valid_moves.map { |move| KnightMove.new(move) }
+    end
+
+    private
+
+    def validate_position(move)
+      move.each { |coord| return false unless (coord >= 0 && coord < 8) }
+      true
+    end
+  end
 end
